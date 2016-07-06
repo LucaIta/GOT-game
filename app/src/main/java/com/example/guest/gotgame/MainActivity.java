3398a06d2d73405db9b8625c47b3450f56399f78
Luca Quatela <lucaqq@gmail.com>
2016-07-05 17:32:02 -0700

    initial files

---
+++ app/src/main/java/com/example/guest/gotgame/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.guest.gotgame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id. playButton) Button mPlayButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mPlayButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
+
+
+}
